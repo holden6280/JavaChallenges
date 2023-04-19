0b63c2d79dd525c13093e5679f9f1782debf61f6
holden6280 <emiicleghorn@gmail.com>
2023-04-19 13:19:58 -0400

    added new challenges

--- src/main/java/org/yup/Challenges.java
+++ src/main/java/org/yup/Challenges.java
@@ -1,20 +1,15 @@
 package org.yup;
-
 import java.util.Scanner;
 
 public class Challenges {
-
     public String greeting(String name, int age){
 
         if(age >= 21){
-
             return "Hello, " + name + ", welcome to the party!";
 
         }else{
            return "Hello, " + name + ", I'm sorry, but we cannot let you into the party.";
-       }
-    }
-
+       }}
     public String greetingWithUserInput(){
 
         Scanner theScanner  = new Scanner(System.in);
@@ -30,15 +25,12 @@ public class Challenges {
         //let folks into the party if they are 21 and up
         //politely tell them that the cant enter underage
         if(age >= 21){
-
             return "Hello, " + name + ", welcome to the party!";
 
         }else{
             //this is what happens if the condition is false
             return "Hello, " + name + ", I'm sorry, but we cannot let you into the party.";
-        }
-    }
-
+        }}
     public int addTwoNumbers (){
 
         Scanner theScanner  = new Scanner(System.in);
@@ -52,12 +44,8 @@ public class Challenges {
         int number2 = theScanner.nextInt();
 
         return number1 + number2;
-
-
     }
-
     public int subtractTwoNumbers (){
-
         Scanner theScanner  = new Scanner(System.in);
 
         //ask the user for number1
@@ -69,13 +57,9 @@ public class Challenges {
         int number2 = theScanner.nextInt();
 
         return number1 - number2;
-
     }
-
     public int multiplyTwoNumbers (){
-
         Scanner theScanner = new Scanner(System.in);
-
 
         //ask the user for number1
         System.out.print("Give me a number please.");
@@ -86,7 +70,162 @@ public class Challenges {
         int number2 = theScanner.nextInt();
 
         return number1 * number2;
+}
+    public int interactiveCalculator() {
+     //fire up scanner so we can use it
+     Scanner theScanner = new Scanner(System.in);
 
+     // displays some set of choices to the user
+     System.out.println("Welcome to the calculator!");
+     System.out.println("Press 1 to add numbers");
+     System.out.println("Press 2 to subtract numbers");
+     System.out.println("Press 3 to multiply numbers");
+     System.out.print("Enter your selection and press ENTER");
+
+     //this sits and waits for the user to give (1,2,3).
+     //when they hit enter their selection is stored in userSelection
+     int userSelection = theScanner.nextInt();
+
+     //creating a variable to store the result of the math
+     int result;
+
+     //
+     switch (userSelection) {
+         case 1:
+             result = this.addTwoNumbers();
+             break;
+         case 2:
+             result = this.subtractTwoNumbers();
+             break;
+         case 3:
+             result = this.multiplyTwoNumbers();
+             break;
+         default:
+             System.out.println("The user chose nonsense(didn't exist as a choice)");
+             return 0;
+     }
+
+     return result;
+
+ }
+    public String coinFlip() {
+
+        //attempt to flip a coin
+        //return heads or tails
+
+        //this is where we flip the coin
+        if (Math.random() > .5) {
+            //here is where we return heads in the number generated was > .5
+            return "heads";
+        } else {
+            //there is where we return if the number generated was less than .5
+            return "tails";
+        }
+    }
+    public String findTheBiggestNumber(){
+
+        //this is the scanner that lets us take input from the user
+        Scanner theScanner  = new Scanner(System.in);
+
+        //this is where we ask the user for input
+        System.out.print("Give me the first number please:");
+        //this is where we capture that input and store it in a variable called firstNumber
+        int firstNumber = theScanner.nextInt();
+
+        //this is where we ask the user for input
+        System.out.print("Now give me the second number please:");
+        //this is where we capture that input and store it in a variable called secondNumber
+        int secondNumber = theScanner.nextInt();
+
+        int theBiggestNumber = Math.max(firstNumber, secondNumber);
+
+        return "The biggest number between " + firstNumber + " and " + secondNumber + " is: " + theBiggestNumber;
 
     }
+    public String findTheSmallestNumber(){
+
+        //ref the above method to make one that finds the smallest of 2 numbers
+
+            //this is the scanner that lets us take input from the user
+            Scanner theScanner  = new Scanner(System.in);
+
+            //this is where we ask the user for input
+            System.out.print("Give me the first number please:");
+            //this is where we capture that input and store it in a variable called firstNumber
+            int firstNumber = theScanner.nextInt();
+
+            //this is where we ask the user for input
+            System.out.print("Now give me the second number please:");
+            //this is where we capture that input and store it in a variable called secondNumber
+            int secondNumber = theScanner.nextInt();
+
+            int theSmallestNumber = Math.min(firstNumber, secondNumber);
+
+            return "The smallest number between " + firstNumber + " and " + secondNumber + " is: " + theSmallestNumber;
+
+        }
+    public String findTheDifferenceBetweenTwoNumbers(){
+
+        Scanner theScanner  = new Scanner(System.in);
+
+        //this is where we ask the user for input
+        System.out.print("Give me the first number please:");
+        //this is where we capture that input and store it in a variable called firstNumber
+        int firstNumber = theScanner.nextInt();
+
+        //this is where we ask the user for input
+        System.out.print("Now give me the second number please:");
+        //this is where we capture that input and store it in a variable called secondNumber
+        int secondNumber = theScanner.nextInt();
+
+        int diff;
+
+        if (firstNumber < secondNumber){
+
+            diff = secondNumber - firstNumber;
+
+        }else{
+
+            diff = firstNumber - secondNumber;
+
+        }
+        return "The difference between " + firstNumber + " and " + secondNumber + " is: " + diff;
+
+    }
+
+    public String convertToUppercase(){
+
+        Scanner theScanner = new Scanner(System.in);
+
+        System.out.print("Provide a word to convert to all UPPERCASE: ");
+        String theWord = theScanner.nextLine();
+
+        return "You provided " + theWord + " and we converted it to: " + theWord.toUpperCase();
+
+    }
+
+    //convertToLowercase - works like convertToUppercase and return some text that shows us the word is converted
+    public String convertToLowercase(){
+
+        Scanner theScanner = new Scanner(System.in);
+
+        System.out.print("Provide a word to convert to all LOWERCASE: ");
+        String theWord = theScanner.nextLine();
+
+        return "You provided " + theWord + " and we converted it to: " + theWord.toLowerCase();
+
+    }
+
+    //lengthOfWord - return sth that lets us know how long the word provided was/is
+    public String length(){
+
+        Scanner theScanner = new Scanner(System.in);
+
+        System.out.print("Enter the text you would like the character count for: ");
+        String text = theScanner.nextLine();
+
+        return "The character count for " + text + " is " + text.length();
+
+    }
+
 }
--- src/main/java/org/yup/JavaChallengesApp.java
+++ src/main/java/org/yup/JavaChallengesApp.java
@@ -4,31 +4,54 @@ public class JavaChallengesApp {
 
     public static void main (String[] args){
 
-        //this code is running greeting
+//this code is running greeting
 
         //create a new instance of the challenges class, so we can use the challenges in our application
         Challenges challenges = new Challenges();
+/*
+//this runs the greeting method from our challenges and returns a string greeting
 
-        //this runs the greeting method from our challenges and returns a string greeting
         //and stores it in the greeting variable we created
         String greeting = challenges.greeting("Eric",20);
 
         //this prints the greeting to the console
         System.out.println(greeting);
 
-        //code below is running greetingWithUserInput()
+//code below is running greetingWithUserInput()
 
         String greetingWithUserInput = challenges.greetingWithUserInput();
 
         System.out.println(greetingWithUserInput);
 
+//calculator challenges below
 
-
+        int sumOfTwoNumbers = challenges.addTwoNumbers();
         System.out.println(challenges.addTwoNumbers());
 
+        int diffOfTwoNumbers = challenges.subtractTwoNumbers();
         System.out.println(challenges.subtractTwoNumbers());
 
+        int prodOfTwoNumbers = challenges.multiplyTwoNumbers();
         System.out.println(challenges.multiplyTwoNumbers());
+
+        int iCresult = challenges.interactiveCalculator();
+        System.out.println(iCresult);
+
+        System.out.println(challenges.coinFlip() );
+
+        System.out.println(challenges.findTheBiggestNumber());
+
+        System.out.println(challenges.findTheSmallestNumber());
+
+        System.out.println(challenges.findTheDifferenceBetweenTwoNumbers());
+
+        System.out.println(challenges.convertToUppercase());
+
+        */
+
+        System.out.println(challenges.convertToLowercase());
+
+        System.out.println(challenges.length());
 
     }
 }
